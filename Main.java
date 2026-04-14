e34f52cbef696c1d8a5276787401df90e1fdbb20
Daniel Bitencourt <bitencourtdaniel30@gmail.com>
2026-04-13 21:38:29 -0300

    Update

---
+++ Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean flag = true;
+        while (flag) {
+            System.out.println("=========== Sistema de Folha de Pagamento ===========");
+            System.out.println("1 - Cadastrar Funcionário Padrão");
+            System.out.println("2 - Cadastrar Funcionário Comissionado");
+            System.out.println("3 - Cadastrar Funcionário Produção");
+            System.out.println("4 - Gerar Folha de Pagamento");
+            System.out.println("0 - Sair");
+            System.out.println("Selecione uma opção:");
+            Scanner leitor = new Scanner(System.in);
+            int opcao;
+            try {
+                opcao = leitor.nextInt();
+
+                if (opcao < 0) {
+                    System.out.println("Digite apenas número válidos!");
+                    continue;
+                }
+            } catch (Exception numException) {
+                // TODO: handle exception
+                System.out.println("Digite apenas número válidos!");
+                continue;
+            }
+            switch (opcao) {
+                case 1:
+                    break;
+
+                case 0:
+                    flag = false;
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+    }
+}
